2a12e497b2399d2782c7082a3e4946df8d0f8732
Max Kelly Mahon <maxkellymahon@gmail.com>
2024-08-14 15:09:52 +0000

    deploy settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-maxxkm-djangoblog-2il4qr4fg34.ws-eu115.gitpod.io',
                 '.herokuapp.com']
